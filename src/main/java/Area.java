48e600e3ad6457b1d0504cc7b9fdb08dfa932c43
Maciej Sawka <maciejsawka@gmail.com>
2019-12-04 15:40:11 +0100

    Add class Area for selecting a group of pixels
    Add abstract class Area with abstract method contains() which
    checks whether point with coordinates (x, y) is contained within
    some defined area.
    
    Provide class RectangleArea, a subtype of Area, which represents
    all pixels located inside of a rectangle, implementing contains()
    accordingly.
    
    To prove that performing contains() for every pixel in a picture
    does not waste too much time, add a simple time measurement in main(),
    performing 1920x1080 method calls and displaying time.

---
+++ src/main/java/Area.java
@@ -0,0 +1,3 @@
+public abstract class Area {
+    public abstract boolean contains(int x, int y); // checks whether the area contains a pixel (x, y)
+}
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,5 +1,19 @@
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // time measurement
+        RectangleArea a = new RectangleArea(0, 0, 1920, 1080); // RectangleArea containing all pixels
+
+        long start = System.nanoTime();
+        for(int i = 0; i < 1920; i++) { // iterating over 1920x1080 pixels
+            for(int j = 0; j < 1080; j++) {
+                boolean res = a.contains(i, j);
+            }
+        }
+        long end = System.nanoTime();
+
+        float timeInMs = ((float) (end - start)) / 1000000.0f;
+
+        System.out.println("Time measurement for 1920x1080 rectangular selection - performing contains() on every pixel");
+        System.out.println(Float.toString(timeInMs) + " ms");
     }
 }
