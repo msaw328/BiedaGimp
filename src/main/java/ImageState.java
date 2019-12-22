3c57c54dadb01adaf3dc96858f1666c696d67bec
MikolajDuda <duda.mikoaj@gmail.com>
2019-12-22 22:34:20 +0100

    Modify class ImageState to represent current state of image
    Add methods getWidth(), getHeight(), getBuffer that returns
    respectively width, height and bytes buffer of image and method
    getPixel() which returns pixel with x and y coordinates and
    throws an error if x or y have wrong values.
    
    Add method setBuffer() which overwrites the bytes buffer with
    other given buffer and method setPixel() which sets new value to pixel
    with x and y coordinates and throws an error if x or y have wrong values.
    
    To prove that above methods work properly, modify Project::main()
    to print results of using them.

--- src/main/java/ImageState.java
+++ src/main/java/ImageState.java
@@ -1,5 +1,47 @@
 public class ImageState {
-    public static void hello() {
-        System.out.println("Hello Maciek!");
+    private int width;      //width of image
+    private int height;     //height of image
+    private byte[] buffer;  //bytes buffer of image
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public byte[] getPixel(int x, int y) {
+        if (x >= width || y >= height) {    //throw an error if x or y have wrong values
+            throw new IllegalArgumentException("x must be less than or equal to width and y must be less than or equal to height");
+        }
+        int loc = 4 * (x + y * width);      //multiple by 4 because each pixel contains 4 channels
+        byte[] pixel = {buffer[loc], buffer[loc + 1], buffer[loc + 2], buffer[loc + 3]};
+        return pixel;
+    }
+
+    public byte[] getBuffer() {
+        return buffer;
+    }
+
+    public void setPixel(int x, int y, byte red, byte green, byte blue, byte alpha) {
+        if (x >= width || y >= height) {     //throw an error if x or y have wrong values
+            throw new IllegalArgumentException("x must be less than or equal to width and y must be less than or equal to height");
+        }
+        int loc = 4*(x + y * width);         //multiple by 4 because each pixel contains 4 channels
+        this.buffer[loc] = red;              //setting each channel to given value
+        this.buffer[loc + 1] = green;
+        this.buffer[loc + 2] = blue;
+        this.buffer[loc + 3] = alpha;
+    }
+
+    public void setBuffer(byte[] buffer) {
+        this.buffer = buffer;
+    }
+
+    public ImageState(int width, int height, byte[] buffer) {
+        this.width = width;
+        this.height = height;
+        this.buffer = buffer;
     }
 }
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,5 +1,26 @@
 public class Program {
+
     public static void main(String[] args) {
-        ImageState.hello();
+        byte[] bytes = new byte[] {100, 100, 100, 100, 101, 101, 101, 101, 102, 102, 102, 102, 103, 103, 103, 103, 104, 104, 104, 104, 105, 105, 105, 105};
+        ImageState imageState = new ImageState(2, 3, bytes);   //create new ImageState
+
+        String r = new String(imageState.getBuffer());
+        System.out.println("Buffer : " + r + "\n");
+
+        String s = new String(imageState.getPixel(1, 1));
+        System.out.println("Pixel at x = 1 and y = 1 : " + s + "\n");
+
+        byte byte1 = 80;
+        imageState.setPixel(1, 1, byte1, byte1, byte1, byte1);       //set pixel at x = 1 and y = 1 to value 80, 80, 80, 80
+
+        String u = new String(imageState.getBuffer());
+        System.out.println("Buffer after change : " + u + "\n");
+
+        String t = new String(imageState.getPixel(1, 1));
+        System.out.println("Pixel at x = 1 and y = 1 after change : " + t + "\n");
+
+        System.out.println("Height : " + imageState.getHeight() + "\n");
+
+        System.out.println("Width : " + imageState.getWidth() + "\n");
     }
 }
