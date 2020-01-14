549a4beed9398ce931421d3470553988939cfad6
luzblu3s <luzblu3s@gmail.com>
2020-01-14 14:31:04 +0100

    History class keep original image. Add names list
    In History class delete unnecessary getElement method. Add list of names
    of used tranformation. Add new methods like public clear() which can
    remove whole history records. Rewrite push method
    
    In Program.java test new implementation

--- src/main/java/History.java
+++ src/main/java/History.java
@@ -3,21 +3,50 @@ import java.util.List;
 
 public class History{
     private List<ImageState> historyAsList = new LinkedList<>();  //List of ImageState changes
+    private List<String> names = new LinkedList<>();    //List of used transformation
     private final int length = 10; //Constant length of history
+    private ImageState original;    //Variable contains original image
+
+    private void deleteUntil(int index){    //clear history and names lists until meet element at selected index
+        if (historyAsList.size() >= index) {
+            names.subList(index, names.size()).clear();
+            historyAsList.subList(index, historyAsList.size()).clear();
+        }
+    }
+
+    public void clear(){    //clear whole history list
+        deleteUntil(0);
+    }
+
+    public void pushOriginal(ImageState imageState, String name){
+        deleteUntil(0);
+        historyAsList.add(new ImageState(imageState));
+        names.add(name);
+    }
 
     public List<ImageState> asList(){
         return historyAsList;
     }
 
-    public ImageState getElement(int index){    //in case of wrong index return last ImageState record
-        if(index < historyAsList.size()) return historyAsList.get(index);
-        else return historyAsList.get(historyAsList.size() - 1);
+    public List<String> getNames() {
+        return names;
     }
 
-    public void push(ImageState imageState){
+    public ImageState getOriginal() {
+        return original;
+    }
+
+    public void push(int index, ImageState imageState, String name){
         if (historyAsList.size() > length){   //if history is larger than 10 delete the oldest ImageState
             historyAsList.remove(0);
+            names.remove(0);
         }
+        deleteUntil(index + 1); //omit history(index) element
+        names.add(name);
         historyAsList.add(new ImageState(imageState));
+    }
+
+    public History(ImageState imageState){
+        original = new ImageState(imageState);
     }
 }
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -6,12 +6,50 @@ public class Program {
 
     public static void main(String[] args) throws IOException {
 
-        String path = "/home/mikolaj/Pulpit/test.png";
-        String savePath = "/home/mikolaj/Pulpit/NEW_rgb.png";
+        byte[] bytes = new byte[] {100, 100, 100, 100, 101, 101, 101, 101, 102, 102, 102, 102, 103, 103, 103, 103, 104, 104, 104, 104, 105, 105, 105, 105};
+
+        ImageState imageState = new ImageState(2, 3, bytes);   //create new ImageState
+
+        History history = new History(imageState);    //create history object
 
 
-        ImageState image = ImageIOWrap.read(path);                          //read image
 
-        ImageIOWrap.write(savePath, image, BufferedImage.TYPE_4BYTE_ABGR);   //write image
+        byte[] pixel = {80, 81, 82, 83};
+
+        imageState.setPixel(1, 1, pixel);       //set pixel at x = 1 and y = 1 to value of variable pixel
+
+        history.push(1, imageState, "Transform1");
+
+
+
+        byte[] pixel2 = {10, 11, 12, 13};
+
+        imageState.setPixel(1,1,pixel2);
+
+        history.push(2, imageState, "Transform2");
+
+
+
+        //After 3 changes print history array
+        System.out.println("Original");
+        System.out.println(Arrays.toString(history.getOriginal().getBuffer()));
+        System.out.println(history.getNames().get(0));
+        System.out.println(Arrays.toString(history.asList().get(0).getBuffer()));
+        System.out.println(history.getNames().get(1));
+        System.out.println(Arrays.toString(history.asList().get(1).getBuffer()));
+
+
+        //checking cleaning history list
+        history.clear();
+        System.out.println(history.asList().isEmpty());
+
+        //checking pushing and removing items
+        byte[] pixel3 = {120, 121, 122, 123};
+        imageState.setPixel(1,1,pixel3);
+        history.push(0, imageState, "Transform3");
+        System.out.println(history.getNames().get(0));
+        System.out.println(Arrays.toString(history.asList().get(0).getBuffer()));
+        System.out.println(history.getNames().get(1));
+        System.out.println(Arrays.toString(history.asList().get(1).getBuffer()));
     }
 }
