3eef2ae4a98ee0e0d411d2fd6f872573017afee8
Maciej Sawka <maciejsawka@gmail.com>
2020-01-20 00:01:36 +0100

    Fix artifacts, clean up code
    Split sobel code into separate functions, highlight
    parts of algorithm with comments.
    
    Perform operations on values from 0 to 1 for better precision.

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class Program {
 
     public static void main(String[] args) throws IOException {
-        ImageState test = ImageIOWrap.read("/home/mikolaj/Pulpit/frog.png");
+        ImageState test = ImageIOWrap.read("/home/user/Pictures/valvepng");
         Transform sobel = new Sobel();
         ImageState after = sobel.apply(test);
-        ImageIOWrap.write("/home/mikolaj/Pulpit/frog_sobel.png", after, BufferedImage.TYPE_4BYTE_ABGR);
+        ImageIOWrap.write("/home/user/Pictures/valve_sobel.png", after, BufferedImage.TYPE_4BYTE_ABGR);
     }
 }
--- src/main/java/Sobel.java
+++ src/main/java/Sobel.java
@@ -14,37 +14,45 @@ public class Sobel extends Effect {
             {-1.0, 0.0, 1.0}
     };
 
-    private byte[] grayscale(byte[] pix) {
-        double aver = ((pix[1] & 0xFF) + (pix[2] & 0xFF) + (pix[3] & 0xFF)) / 3;
-        pix = new byte[]{pix[0], (byte) aver, (byte) aver, (byte) aver};
-        return pix;
+    // converts a pixel to a value in range <0, 1)
+    private double getAverageToFraction(byte[] pix) {
+        int average = ((pix[1] & 0xFF) + (pix[2] & 0xFF) + (pix[3] & 0xFF)) / 3;
+        return ((double) average) / 256.0;
+    }
+
+    // checks if point x, y is within given bounds
+    private boolean isWithinBounds(int x, int y, int width, int height) {
+        return (x > 0 && x < width) && (y > 0 && y < height);
     }
 
     @Override
     protected byte[] getPixel(int x, int y, int width, int height, ImageState input) {
         double sobelPartH = 0;
         double sobelPartV = 0;
-        double val = 0;
+        double sobelBoth;
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if ((x - 2 > 0 && y - 2 > 0)) {
-                    if (x + 2 < width && y + 2 < height) {
-                        sobelPartH += sobelMatH[j][i] * (((double) grayscale(input.getPixel(x + i - 1, y + j - 1))[1]) / 256.0);
-                        sobelPartV += sobelMatV[j][i] * (((double) grayscale(input.getPixel(x + i - 1, y + j - 1))[1]) / 256.0);
-                    }
-                }
+                int currentX = x + i - 1;
+                int currentY = y + j - 1;
+
+                if(!isWithinBounds(currentX, currentY, width, height)) continue;
+
+                double val = getAverageToFraction(input.getPixel(currentX, currentY));
+
+                sobelPartH += sobelMatH[j][i] * val;
+                sobelPartV += sobelMatV[j][i] * val;
             }
         }
 
-        val = Math.sqrt((sobelPartH * sobelPartH) + (sobelPartV * sobelPartV));
+        sobelBoth = Math.sqrt((sobelPartH * sobelPartH) + (sobelPartV * sobelPartV));
 
-        if (val * 256.0 > 255.0)
-            val = 255.0;
+        // threshholding
+        if (sobelBoth * 256.0 > 255.0)
+            sobelBoth = 255.0;
         else
-            val = val * 256.0;
+            sobelBoth = sobelBoth * 256.0;
 
-        byte[] pixel = new byte[]{input.getPixel(x, y)[0], (byte) val, (byte) val, (byte) val};
-        return pixel;
+        return new byte[]{input.getPixel(x, y)[0], (byte) sobelBoth, (byte) sobelBoth, (byte) sobelBoth};
     }
 }
