fe66b6273d0e94ce5fc1f0f92124ef03eca69877
Maciej Sawka <maciejsawka@gmail.com>
2020-01-20 13:19:04 +0100

    Implement multiple iterations of transforms

--- src/main/java/Transform.java
+++ src/main/java/Transform.java
@@ -1,25 +1,38 @@
 // Transform is the most abstract type which describes a way that
 // one ImageState can be turned into another
 
+import java.awt.*;
+
 public abstract class Transform {
     protected abstract byte[] getPixel(int x, int y, int width, int height, ImageState input);
     protected abstract int getOutputWidth(ImageState input);
     protected abstract int getOutputHeight(ImageState input);
 
     public final ImageState apply(ImageState input) {
-        int width = getOutputWidth(input);
-        int height = getOutputHeight(input);
-        byte[] data = new byte[width * height * 4];
+        return this.apply(input, 1);
+    }
 
-        ImageState output = new ImageState(width, height, data);
+    public final ImageState apply(ImageState input, int iterations) {
+        ImageState partialIn = input;
+        ImageState partialOut;
 
-        for(int x = 0; x < width; x++) {
-            for(int y = 0; y < height; y++) {
-                byte[] pixel = getPixel(x, y, width, height, input);
-                output.setPixel(x, y, pixel);
+        for(int i = 0; i < iterations; i++) { // in each iteration
+            int width = getOutputWidth(partialIn);
+            int height = getOutputHeight(partialIn);
+            byte[] buffer = new byte[width * height * 4];
+
+            partialOut = new ImageState(width, height, buffer); // create new output imagestate
+
+            for (int x = 0; x < width; x++) { // apply transform from partialIn to partialOut
+                for (int y = 0; y < height; y++) {
+                    byte[] pixel = getPixel(x, y, width, height, partialIn);
+                    partialOut.setPixel(x, y, pixel);
+                }
             }
+
+            partialIn = partialOut; // set partialIn for next iteration as output of this iteration
         }
 
-        return output;
+        return partialIn; // after all iterations, result is in partialIn
     }
 }
